8953bf2fbbe77df5943037e200e8e9c4f2dab274
Nyi Ye Naing <guesszero24@gmail.com>
2026-04-10 20:57:59 +0700

    add: cors config

---
+++ config/cors.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://zarmani108.uk',
+        'https://www.zarmani108.uk',
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:3000',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
