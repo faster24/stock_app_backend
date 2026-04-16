586c0c6d2cee2c1dec30827542fe49aa97c55bb9
Nyi Ye Naing <guesszero24@gmail.com>
2026-04-16 20:50:32 +0700

    feat(enums): add Thai banks to BankName

--- app/Enums/BankName.php
+++ app/Enums/BankName.php
@@ -9,5 +9,12 @@ enum BankName: string
     case CB = 'CB';
     case UAB = 'UAB';
     case YOMA = 'YOMA';
+    case SCB = 'SCB';
+    case KBANK = 'KBANK';
+    case BBL = 'BBL';
+    case KTB = 'KTB';
+    case BAY = 'BAY';
+    case TTB = 'TTB';
+    case GSB = 'GSB';
     case OTHER = 'OTHER';
 }
